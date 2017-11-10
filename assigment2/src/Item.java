2075d8b67a5ce875d8a3aeaf409048e41c381023
xueqi <s1719180@ed.ac.uk>
2017-11-10 21:07:26 +0000

    initial

---
+++ assigment2/src/Item.java
@@ -0,0 +1,38 @@
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Item {
+	@FXML
+	private ImageView item1View;
+	
+	@FXML
+	private ImageView item2View;
+	
+	@FXML
+	private ImageView basketView;
+	
+	private Image basket = new Image("basket.png");
+	private int itemNum = 0;
+	
+	public void putItem() {
+		
+		basketView.setImage(basket);        
+        basketView.setVisible(true);
+        item1View.setVisible(false);
+        itemNum--;
+	}
+	
+	public void pickItem() {
+		basketView.setVisible(true);
+		if (itemNum == 0) {
+			item1View.setImage(basket);                
+	        item1View.setVisible(false);
+	        itemNum++;
+		} else {
+			item2View.setImage(basket);                
+	        item2View.setVisible(false);
+		}
+		
+	}
+}
